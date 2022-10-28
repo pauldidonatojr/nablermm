5dc2984bc94ad73545f91b2e93d0ca45d7e42ebd
PJ <87795990+pauldidonatojr@users.noreply.github.com>
2022-10-28 19:46:26 -0400

    push me 1

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,31 +1,30 @@
 import React from 'react'
 import styled from 'styled-components'
-import {Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 function Hero() {
-    const url = 'https://tinyurl.com/4ubxa88e'
-    return (
-     <Wrapper>
-      <div className="container">
-       <Link to={{ pathname: 'https://tinyurl.com/4ubxa88e' }} target="_blank">
-        Login
-       </Link>
-      </div>
-     </Wrapper>
-    )
+ return (
+  <Wrapper>
+   <div className="container">
+    <Link to={{ pathname: 'https://tinyurl.com/4ubxa88e' }} target="_blank">
+     Login
+    </Link>
+   </div>
+  </Wrapper>
+ )
 }
 
 const Wrapper = styled.div`
-   height: 75vh;
-   padding: 2rem;
-   border: 2px black solid;
-   
-   display: grid;
-   justify-content: center;
-   align-items: center;
-   .container {
-    background-color: lightgrey;
-    border: 5px black solid;
-    padding: 15rem;
-   }
+ height: 75vh;
+ padding: 2rem;
+ border: 2px black solid;
+
+ display: grid;
+ justify-content: center;
+ align-items: center;
+ .container {
+  background-color: lightgrey;
+  border: 5px black solid;
+  padding: 15rem;
+ }
 `
 export default Hero
